3b0eecaa9d802e3a58d2610a7455dc6c5bf3c538
nsssayom <nsssayom@gmail.com>
2018-11-13 16:29:11 +0600

    Ditching ATOM

--- class/book.php
+++ class/book.php
@@ -1,27 +1,78 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/LibraryDeCentral/functions/validator.php');
- class Book{
+
+class book
+{
     private $Database;
-    private $id;
-    private $title;
 
     public function __construct($DatabaseLink)
     {
         $this->Database = $DatabaseLink;
     }
 
-    public function getId(){
-        return $id;
+    public function loadBookInfo($id)
+    {
+
+        if (empty($id)) return false; //or does it need status code?
+
+        $id = $this->Database->escape($id);
+
+        $sql = "SELECT * FROM book WHERE id='$id'";
+        $result = $this->Database->getArray($sql);
+
+        if (!mysqli_num_row($result)) { //if mysqli_num_row not zero
+
+            return json_encode($result);
+
+        } else return; //status code?
     }
 
-    public function getTitle(){
+    public function setBookInfo($param)
+    {
+        $params = array();
+        foreach ($param as $key => $value) {
+            $params[$key] = $this->Database->escape($value);
+        }
 
+        $data = array();
+        $data['status'] = "330";
+        $response = json_encode(array($data));
+
+        if (!isset($params['bookid']) &&
+            !isset($params['title']) &&
+            !isset($params['author'])) {
+
+            return $response;
+        }
     }
-    public function setTitle($str){
 
-        $this->$title=$this->Database->escape($str);
-        $sql="INSERT INTO `book` (`id`, `title`, `edition`, `isbn`, `pub_id`) VALUES ('$title')";
-        $this->$Database->query($sql);
-        
+    private function setGenre($string)
+    {
+        $sql = "SELECT id FROM genre WHERE name='$string'";
+        $result = $this->Database->getArray($sql);
+        if (isset($result[0])) {
+            //genre found and id returned
+            return $result['id'];
+        } else {
+            //genre not found
+            $sql = "INSERT INTO genre(name) VALUES('$string')";
+            if ($this->Database->query($sql)) {
+                $sql = "SELECT LAST_INSERT_ID()";
+                $result = $this->Database->query($sql);
+                if (isset($result[0])) {
+                    return $result['id'];
+                } else {
+                    $data = array();
+                    $data['status'] = "400";
+                    $response = json_encode(array($data));
+                    return $response;
+                }
+            } else {
+                $data = array();
+                $data['status'] = "400";
+                $response = json_encode(array($data));
+                return $response;
+            }
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
-  include_once($_SERVER['DOCUMENT_ROOT'].'/LibraryDeCentral/class/database.php');
-  include_once($_SERVER['DOCUMENT_ROOT'].'/LibraryDeCentral/class/user.php');
-  include_once($_SERVER['DOCUMENT_ROOT'] . '/LibraryDeCentral/functions/validator.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/LibraryDeCentral/class/database.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/LibraryDeCentral/class/user.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/LibraryDeCentral/functions/validator.php');
 
-  $dbConfigStr = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/LibraryDeCentral/config/database.json');
-  $dbConfig = json_decode($dbConfigStr, true);
+$dbConfigStr = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/LibraryDeCentral/config/database.json');
+$dbConfig = json_decode($dbConfigStr, true);
 
-  $database = new Database($dbConfig['host'], $dbConfig['username'], $dbConfig['password'], $dbConfig['database']);
-  $database->connect();
-  $user = new User($database);
+$database = new Database($dbConfig['host'], $dbConfig['username'], $dbConfig['password'], $dbConfig['database']);
+$database->connect();
+$user = new User($database);
+$book = new Book($database);
+//echo $user->login("admin@softopian.com","1234");
+echo $user->register("Arnika", "4621", "+8801750252963", "arnikaroyrumu@gmail.com", "Arnika Roy Rumu", "1997-09-13");
+//echo $user->verifyToken("485e8ab041713413d466ffd99d1d93afdfa4dae5fa2850f839a474f0e8d118cd");
+//echo (int) ($user->isUserNameAvailable("nsssayom"));
+//echo (int) ($user->isUserNameAvailable("admin"));
 
-  //echo $user->login("admin@softopian.com","1234");
-  echo $user->register("Arnika", "4621", "+8801750252963", "arnikaroyrumu@gmail.com", "Arnika Roy Rumu", "1997-09-13");
-  //echo $user->verifyToken("485e8ab041713413d466ffd99d1d93afdfa4dae5fa2850f839a474f0e8d118cd");
-  //echo (int) ($user->isUserNameAvailable("nsssayom"));
-  //echo (int) ($user->isUserNameAvailable("admin"));
+//echo  ($user->isEmailAvailable("admin@softopian.com"));
+//echo  ($user->isEmailAvailable("nsssayom@softopian.com"));
 
-  //echo  ($user->isEmailAvailable("admin@softopian.com"));
-  //echo  ($user->isEmailAvailable("nsssayom@softopian.com"));
+//echo (int) ($user->isPhoneAvailable("+8801717018376"));
+//echo (int) ($user->isPhoneAvailable("8801521308829"));
 
-  //echo (int) ($user->isPhoneAvailable("+8801717018376"));
-  //echo (int) ($user->isPhoneAvailable("8801521308829"));
-
-  //$user->register("admin", "dd", "+8801717018376", "admin@softopian.com");
+//$user->register("admin", "dd", "+8801717018376", "admin@softopian.com");
 ?>
