df21716f13c92c9f24c250c21585567b0de235c9
Sayom Shakib <nsssayom@gmail.com>
2018-11-24 14:36:10 +0600

    .

--- api/remove_author.php
+++ api/remove_author.php
@@ -16,10 +16,10 @@ if (isset($_POST['token']) && !empty($_POST['token']) &&
     $userID = $user->verifyToken($_POST['token']);
 
     $book_id = $_POST['bookId'];
-    $sql = "SELECT `user_id` FROM book_user WHERE book_id = '$book_id'";
+    $sql = "SELECT book_user.user_id, user.privilege FROM book_user INNER JOIN user ON book_user.user_id=user.id WHERE book_user.book_id='$book_id'";
     $result = $database->getArray($sql);
 
-    if ($result['user_id'] == $userID){
+    if ($result['user_id'] == $userID||$result['privilage']<4){
         $book->removeAuthor($_POST['bookId'], $_POST['authorId']);
     }
     else{
